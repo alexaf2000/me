e141162d89bf383afc329fc1f589211c5a5bce75
Alex A.F <alex2andres@gmail.com>
2020-08-12 21:28:17 +0200

    Add example API

--- alex_andres/urls.py
+++ alex_andres/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from api.views import book
+
 urlpatterns = [
+    path("book/", book, name="book"),
     path('admin/', admin.site.urls),
 ]
