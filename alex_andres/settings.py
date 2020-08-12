ee7802e86f6853db02b9d224ebe893df44e36647
Alex A.F <alex2andres@gmail.com>
2020-08-12 21:22:40 +0200

    Add api and core apps and rest_framework

--- alex_andres/settings.py
+++ alex_andres/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    # Our webiste
+    'api',
+    'core',
+
 ]
 
 MIDDLEWARE = [
